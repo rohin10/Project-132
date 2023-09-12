af9bc07c0a86d53c43a8c0196ca1f1baacde8d0e
rohin10 <104300062+rohin10@users.noreply.github.com>
2023-09-12 13:38:27 +0530

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,29 @@
+img="";
+status1="";
+function preload(){
+    img = loadImage("fruitbasket.jpg");
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.position(440,240);
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    document.getElementById("status").innerHTML = "Status : Objects Detected";
+}
+function draw(){
+    image(img,0,0,640,420);
+    
+}
